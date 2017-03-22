898a6f00c98f276bc343ccf5ab3a3e3eac2e1515
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2017-03-22 12:15:32 -0600

    edited redirect.php to display alert message

--- web/myapp/redirect.php
+++ web/myapp/redirect.php
@@ -1,7 +1,13 @@
 <?php
+try{
 if (isset($_GET['Message'])) {
-    print $_GET['Message'];
+    echo '<script language="javascript">';
+    echo 'alert('.$_GET['Message'].')';
 }
 header('Location: login.html');
 exit();
+}
+catch (PDOException $e){
+    echo $e->getMessage();
+}
 ?>
